879740ad7026f651f8404b54bce1ad1966f66932
valtimperiumgit <valtimperium@gmail.com>
2023-03-02 01:37:31 +0200

    Add Lobby.Logic, User entity

---
+++ Lobby.Logic/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Lobby.Logic;
+
+public class Class1
+{
+}
---
+++ Lobby.Models/Entities/User/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Lobby.Models.Entities.User;
+
+public class User
+{
+    public Guid Id { get; set; }
+    
+    public string Alias { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+    
+    public DateTime Created { get; set; }
+    
+    public DateTime LastLogin { get; set; }
+    
+    public bool EmailVerificated { get; set; }
+    
+    public string Icon { get; set; }
+}
